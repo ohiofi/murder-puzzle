46dabeaa1c34a4535d1c83df9fe86a068c09ed07
Justin Riley <20hz20khz@gmail.com>
2025-03-17 13:41:18 -0400

    Update Person.js

--- vanillajs/Person.js
+++ vanillajs/Person.js
@@ -1,3 +1,15 @@
+const possibleNames = [
+    ["Amy", "Alex", "Ava", "Allan", "Andy", "Abby", "Alex", "Amir", "Anya", "Aria", "Axel", "Aden", "Alia"],
+    ["Ben", "Bill", "Bob", "Beau", "Brad", "Bryn", "Bret", "Blake", "Basil", "Bodie","Beth","Betty","Barb","Beth"],
+    ["Cal", "Carl", "Cody", "Cory", "Clara", "Clay", "Cole", "Case", "Chet", "Chad", "Cara", "Cami", "Cleo", "Cali", "Cate", "Cori", "Cindy"],
+    ["Dan", "Dave", "Dale", "Dean", "Drew", "Dion", "Dane", "Davy",  "Dana", "Demi", "Dina", "Dora", "Dawn", "Dixie", "Dolly"],
+    ["Eli", "Evan", "Earl", "Eric", "Eddy", "Emma", "Ella", "Elsa", "Erin", "Eve", "Edie", "Emmy"],
+]
+
 class Person{
-    
+    constructor(_name, _eyeColor, _handed){
+        this.name = _name;
+        this.eyeColor = _eyeColor;
+        this.handed = _handed;
+    }
 }
